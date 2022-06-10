b55220175c51a852d271e8aa1445943cad54c5d9
tahaaytekin <tahaaytekin@imaginitesoft.com>
2022-06-10 15:13:55 +0300

    Kapılarda Düzenleme

--- Assets/[Scripts]/Gate.cs
+++ Assets/[Scripts]/Gate.cs
@@ -4,25 +4,81 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
+public enum GateType { BLUE, PURPLE, RED, GREEN };
 public class Gate : MonoBehaviour
 {
+    public GateType myGate;
+    public MeshRenderer planeObject;
+    public Color blueMaterial, purpleMaterial, redMaterial, greenMaterial;
+    private void Start()
+    {
+        StartColorChange();
+    }
+    public void StartColorChange()
+    {
+        if (myGate == GateType.BLUE)
+        {
+            planeObject.materials[0].color = blueMaterial;
+        }
+        else if (myGate == GateType.PURPLE)
+        {
+            planeObject.materials[0].color = purpleMaterial;
+        }
+        else if (myGate == GateType.RED)
+        {
+            planeObject.materials[0].color = redMaterial;
+        }
+        else if (myGate == GateType.GREEN)
+        {
+            planeObject.materials[0].color = greenMaterial;
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-          //  print("gate'e girdi");
+            //  print("gate'e girdi");
             Player p = other.GetComponentInParent<Player>();
-            p.myType = ColourType.BLUE;
+            CharacterColorChanges(other);
+            MMVibrationManager.Haptic(HapticTypes.LightImpact, true, this);
+            GameManager.Intance.audioSource.PlayOneShot(GameManager.Intance.gateSound);
+            gameObject.SetActive(false);
+
+        }
+    }
+    public void CharacterColorChanges(Collider other)
+    {
+        if (myGate == GateType.BLUE)
+        {
+            Player.Instance.myType = ColourType.BLUE;
             GameManager.Intance.oldType = ColourType.BLUE;
             Player.Instance.mesh.materials[0].color = GameManager.Intance.color[0];
             Player.Instance.mesh.materials[1].color = GameManager.Intance.color[0];
             Player.Instance.mesh.materials[2].color = GameManager.Intance.color[0];
-            MMVibrationManager.Haptic(HapticTypes.LightImpact, true, this);
-            GameManager.Intance.audioSource.PlayOneShot(GameManager.Intance.gateSound);
-          //  gameObject.transform.DOScale(Vector3.zero, 0.15f).SetEase(Ease.Linear).OnComplete(()=> 
-            //{
-                gameObject.SetActive(false);
-          //  });
+        }
+        else if (myGate == GateType.PURPLE)
+        {
+            Player.Instance.myType = ColourType.PURPLE;
+            GameManager.Intance.oldType = ColourType.PURPLE;
+            Player.Instance.mesh.materials[0].color = GameManager.Intance.color[1];
+            Player.Instance.mesh.materials[1].color = GameManager.Intance.color[1];
+            Player.Instance.mesh.materials[2].color = GameManager.Intance.color[1];
+        }
+        else if (myGate == GateType.RED)
+        {
+            Player.Instance.myType = ColourType.RED;
+            GameManager.Intance.oldType = ColourType.RED;
+            Player.Instance.mesh.materials[0].color = GameManager.Intance.color[2];
+            Player.Instance.mesh.materials[1].color = GameManager.Intance.color[2];
+            Player.Instance.mesh.materials[2].color = GameManager.Intance.color[2];
+        }
+        else if (myGate == GateType.GREEN)
+        {
+            Player.Instance.myType = ColourType.GREEN;
+            GameManager.Intance.oldType = ColourType.GREEN;
+            Player.Instance.mesh.materials[0].color = GameManager.Intance.color[3];
+            Player.Instance.mesh.materials[1].color = GameManager.Intance.color[3];
+            Player.Instance.mesh.materials[2].color = GameManager.Intance.color[3];
         }
     }
 }
